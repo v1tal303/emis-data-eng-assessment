b7f7930e6e42e48dfa62274b728372f5c83d2422
VitaL <vitaldukm@gmail.com>
2022-02-06 14:12:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import pandas as pd
 import json
 from tqdm.auto import tqdm
 
+from fhir.resources.bundle import Bundle
+from fhir.resources.patient import Patient
+from fhir.resources.condition import Condition
+from fhir.resources.observation import Observation
+from fhir.resources.medicationrequest import MedicationRequest
+from fhir.resources.procedure import Procedure
+from fhir.resources.encounter import Encounter
+from fhir.resources.claim import Claim
+from fhir.resources.immunization import Immunization
+
 tqdm.pandas()
 
 # Get all the .json files from the directory below
@@ -24,5 +34,22 @@ ENCOUNTER = pd.DataFrame(
 CLAIM = pd.DataFrame(columns=['ClaimProvider', 'ClaimInsurance', 'ClaimDate', 'ClaimType', 'ClaimItem',
                               'ClaimUSD', 'PatientUID'])
 IMMUNIZATION = pd.DataFrame(columns=['Immunization', 'ImmunizationDates', 'PatientUID'])
+
+
+
+for fileNum in tqdm(range(len(Allfiles))):
+    f = open('/Users/murci/Documents/GitHub/emis-data-eng-assessment/data/'+Allfiles[fileNum], encoding="utf8")
+    json_obj = json.load(f)
+
+    singleBundle = Bundle.parse_obj(json_obj)
+
+    # Resources
+    resources = []
+    if singleBundle.entry is not None:
+        for entry in singleBundle.entry:
+            resources.append(entry.resource)
+
+    singlePatient = Patient.parse_obj(resources[0])
+
 
 
