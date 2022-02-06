43b3d4ee31e8cc2d7096d0602b91fbe23fac59a8
VitaL <vitaldukm@gmail.com>
2022-02-06 15:38:14 +0000

    Update postgresql.py

--- postgresql.py
+++ postgresql.py
@@ -1,7 +1,13 @@
 import psycopg2
+import pandas as pd
+
+data = pd.read_csv ('PATIENT.csv')
+df = pd.DataFrame(data)
+
+
 
 conn = psycopg2.connect(
-   database="postgres",
+   database="fhir",
     user='postgres',
     password='password',
     host='localhost',
@@ -14,11 +20,20 @@ conn.autocommit = True
 cursor = conn.cursor()
 
 # query to create a database
-sql = ''' CREATE database test2 ''';
+# sql = ''' CREATE database fhir '''
+
+cursor.execute('''
+		CREATE TABLE patient (
+			PatientUID varchar(50) primary key,
+			NameFamily varchar(50),
+			NameGiven varchar(50),
+			DoB DATE,
+			Gender varchar(50))
+               ''')
 
 # executing above query
-cursor.execute(sql)
-print("Database has been created successfully !!");
+# cursor.execute(sql)
+print("Database has been created successfully !!")
 
 # Closing the connection
 conn.close()
