aa05549b7b5c1f37d5b66b612f9f0a50d8bcd7f1
VitaL <vitaldukm@gmail.com>
2022-02-06 15:49:38 +0000

    Update postgresql.py

--- postgresql.py
+++ postgresql.py
@@ -1,10 +1,12 @@
 import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 
 data = pd.read_csv ('PATIENT.csv')
 df = pd.DataFrame(data)
 
 
+print(df)
 
 conn = psycopg2.connect(
    database="fhir",
@@ -22,14 +24,20 @@ cursor = conn.cursor()
 # query to create a database
 # sql = ''' CREATE database fhir '''
 
-cursor.execute('''
-		CREATE TABLE patient (
-			PatientUID varchar(50) primary key,
-			NameFamily varchar(50),
-			NameGiven varchar(50),
-			DoB DATE,
-			Gender varchar(50))
-               ''')
+# cursor.execute('''
+# 		CREATE TABLE patient (
+# 			PatientUID varchar(50) primary key,
+# 			NameFamily varchar(50),
+# 			NameGiven varchar(50),
+# 			DoB DATE,
+# 			Gender varchar(50))
+#                ''')
+
+engine = create_engine('postgresql://postgres:password@localhost:5432/fhir')
+
+df.to_sql("patients2", engine, index=False)
+
+conn.commit()
 
 # executing above query
 # cursor.execute(sql)
